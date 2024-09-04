5c990a40d7fcb7468d2c9d4c9afdd4db93ee3a95
cslj <cslj@itu.dk>
2024-09-04 15:15:57 +0200

    test

--- Chirp.CLI.Client/UserInterface.cs
+++ Chirp.CLI.Client/UserInterface.cs
@@ -4,7 +4,6 @@ static class UserInterface
 {
     void PrintCheeps(IEnumerable<Cheep> cheeps)
     {
-        var cheeps = csv.GetRecords<Cheep>();
         foreach (var cheep in cheeps)
         {
             Console.WriteLine(cheep);
