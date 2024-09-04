fd70e8cbd9959e63c3d0bc619baf9852745f4c42
BBSITU <Biancabs@live.dk>
2024-09-04 15:38:36 +0200

    Made class UserInterface.cs Then Refactor print lines from program.cs into UserInterface, so all print process comes from there Then use method in program
    Co-authored-by: Caroline <cslj@itu.dk>

--- Chirp.CLI.Client/UserInterface.cs
+++ Chirp.CLI.Client/UserInterface.cs
@@ -1,6 +1,11 @@
 namespace Chirp.CLI;
-
 static class UserInterface
 {
-    
+    public static void printCheeps(IEnumerable<Cheep> cheeps)
+    {
+        foreach (var cheep in cheeps)
+        {
+            Console.WriteLine(cheep);
+        }
+    }
 }
