5569e0ce2cf4803df61ecd67e15e2691e572ca5e
BBSITU <Biancabs@live.dk>
2024-09-18 09:45:08 +0200

    CSVDatabase with singleton pattern

--- src/SimpleDB/CSVDatabase.cs
+++ src/SimpleDB/CSVDatabase.cs
@@ -3,10 +3,29 @@ using CsvHelper;
 
 namespace SimpleDB;
 
-public class CSVDatabase<T> : IDatabaseRepository<T> {
-    string recordDatabasePath = ".\\data\\chirp_cli_db.csv";
-    
+public sealed class CSVDatabase<T> : IDatabaseRepository<T> {
+    private static string recordDatabasePath = ".\\data\\chirp_cli_db.csv";
 
+    private static CSVDatabase<T> instance = null;
+    private static readonly object singletonLock = new object();
+
+    public static CSVDatabase<T> Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (singletonLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new CSVDatabase<T>();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
     public IEnumerable<T> Read(int? limit = null)
     {
         using (StreamReader sr = new StreamReader(recordDatabasePath))
