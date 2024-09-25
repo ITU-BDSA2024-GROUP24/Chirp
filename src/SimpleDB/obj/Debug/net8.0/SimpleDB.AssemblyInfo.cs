6e5cbb7d065c3370bbb20b4be3a0308f410730a3
fbor <fbor@itu.dk>
2024-09-25 13:39:04 +0200

    pr test

--- src/SimpleDB/obj/Debug/net8.0/SimpleDB.AssemblyInfo.cs
+++ src/SimpleDB/obj/Debug/net8.0/SimpleDB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511298296e6443fd8663b6c0769daf2563bc62d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dba20e9d174ad670b5ff59449bfe18a067f4f9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ test/SimpleDB.Tests/IntegrationsTest.cs
@@ -0,0 +1,17 @@
+﻿namespace SimpleDB.Tests;
+using Xunit;
+
+public class IntegrationsTest
+{
+
+    [Fact]
+    public void IntegrationsTest1() {
+        //?
+        
+    }
+    [Fact]
+    public void IntegrationsTest2() {
+        //?
+        
+    }
+}
