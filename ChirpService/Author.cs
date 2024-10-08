460f763f77b04fdd192b3a0aaacbb35948f3d85d
cslj <cslj@itu.dk>
2024-10-08 21:43:50 +0200

    added constructor to Author

--- ChirpService/Author.cs
+++ ChirpService/Author.cs
@@ -5,5 +5,13 @@ public class Author
     public string Name { get; set; }
     public string Email { get; set; }
     
-    private ICollection<Cheep> _cheeps;     
+    private ICollection<Cheep> _cheeps;
+
+    public Author (string name, string email)
+    {
+        this.Name = name;
+        this.Email = email;
+    }
+    
+    
 }
--- ChirpService/Cheep.cs
+++ ChirpService/Cheep.cs
@@ -5,6 +5,6 @@ public class Cheep
     public string Text { get; set; }
     public DateTime Timestamp { get; set; }
 
-   
-    
+    private ICollection<Author> _authors;
+
 }
