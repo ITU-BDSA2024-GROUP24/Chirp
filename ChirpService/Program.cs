c2148b5499c49ae7a9afa2875d5005e4a30ee638
cslj <cslj@itu.dk>
2024-10-09 15:12:24 +0200

    :w#

--- ChirpService/Program.cs
+++ ChirpService/Program.cs
@@ -4,8 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddSingleton<ICheepService, CheepService>();
-builder.Services.AddScoped<ICheepRepository, CheepRepository>();
+//builder.Services.AddSingleton<ICheepService, CheepService>();
+//builder.Services.AddScoped<ICheepRepository, CheepRepository>();
 
 
 var app = builder.Build();
