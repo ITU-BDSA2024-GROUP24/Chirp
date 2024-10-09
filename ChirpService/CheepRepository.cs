4836c6da67717eafac2a25e6cdb82f7e2500eeea
cslj <cslj@itu.dk>
2024-10-09 09:32:59 +0200

    extended cheepRepo

--- ChirpService/CheepRepository.cs
+++ ChirpService/CheepRepository.cs
@@ -1,6 +1,19 @@
 namespace Chirp.Razor;
 
-public class CheepRepository
+public class CheepRepository : ICheepRepository
 {
-    
+    public Task CreateCheep(CheepDTO newCheep)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<CheepDTO>> ReadCheep(string userName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateCheep(CheepDTO alteredCheep)
+    {
+        throw new NotImplementedException();
+    }
 }
--- ChirpService/Program.cs
+++ ChirpService/Program.cs
@@ -1,8 +1,11 @@
+using Chirp.Razor;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSingleton<ICheepService, CheepService>();
+builder.Services.AddScoped<ICheepRepository, CheepRepository>();
 
 
 var app = builder.Build();
