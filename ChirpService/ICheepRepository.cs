113a3380a4c27d66e210d28bf88ab4d4520cf859
cslj <cslj@itu.dk>
2024-10-09 09:27:33 +0200

    added methods to ICheepRepository

--- ChirpService/ICheepRepository.cs
+++ ChirpService/ICheepRepository.cs
@@ -2,5 +2,9 @@ namespace Chirp.Razor;
 
 public interface ICheepRepository
 {
-    
-}
+
+
+    public Task CreateCheep(CheepDTO newCheep);
+    public Task<List<CheepDTO>> ReadCheep(string userName);
+    public Task UpdateCheep(CheepDTO alteredCheep);
+}                                   
