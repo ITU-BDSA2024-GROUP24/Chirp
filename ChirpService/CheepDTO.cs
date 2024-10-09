11186932f7a27f2c0513b6a410f827a0f27e9e40
cslj <cslj@itu.dk>
2024-10-09 09:43:33 +0200

    added attributes to CheepDTO

--- ChirpService/CheepDTO.cs
+++ ChirpService/CheepDTO.cs
@@ -2,5 +2,9 @@ namespace Chirp.Razor;
 
 public class CheepDTO
 {
+    public string Text { get; set; }
     
+    public long Timestamp { get; set; }
+    
+    public string Author { get; set; }  
 }
