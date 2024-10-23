fe676e51226b2e0407b6dad48ee650132531896f
alju <alexjuhl123@gmail.com>
2024-10-23 12:19:09 +0200

    Roadblock
    The tests should be able to run, but there is a problem with the server (Response code 500) that makes the tests not work

--- ChirpService/Program.cs
+++ ChirpService/Program.cs
@@ -26,3 +26,5 @@ app.UseRouting();
 app.MapRazorPages();
 
 app.Run();
+
+public partial class Program {}
--- Tests/CheepServiceTests/UnitTest1.cs
+++ Tests/CheepServiceTests/UnitTest1.cs
@@ -1,10 +1,41 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Chirp.Razor;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace CheepServiceTests;
 
-public class UnitTest1
+public class UnitTest1 : IClassFixture<WebApplicationFactory<Program>>
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly WebApplicationFactory<Program> _fixture;
+    private readonly HttpClient _client;
 
+    public UnitTest1(WebApplicationFactory<Program> fixture)
+    {
+        _fixture = fixture;
+        _client = _fixture.CreateClient();
     }
+
+    [Fact]
+    public async void CanSeePublicTimeline()
+    {
+        var response = await _client.GetAsync("/");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains("Public Timeline", content);
+    }
+
+    /* [Theory]
+    [InlineData("Helge")]
+    [InlineData("Adrian")]
+    public async void CanSeePrivateTimeline(string author)
+    {
+        var response = await _client.GetAsync($"/{author}");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains($"{author}'s Timeline", content);
+    }*/
 }
