2ef1a1fdebc0bdd47607c6291b4a919a1925c0ec
cslj <cslj@itu.dk>
2024-11-06 14:53:58 +0100

    "added Infrastructure.test"

---
+++ Tests/Chirp.Infrastructure.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Chirp.Infrastructure.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
