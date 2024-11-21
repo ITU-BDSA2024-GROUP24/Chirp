3422672f61ae7dfacd014da78b22ba76d96bbf15
cslj <cslj@itu.dk>
2024-11-21 20:16:45 +0100

    added function to display cheep
    Co-authored-by: BBSITU <biancabs@live.dk>

--- src/Chirp.Web/Pages/Shared/Model/CheepFormatMessage.cs
+++ src/Chirp.Web/Pages/Shared/Model/CheepFormatMessage.cs
@@ -4,5 +4,8 @@ namespace Chirp.Web.Pages.Shared.Model;
 
 public class CheepFormatMessage
 {
-    
+    [Required]
+    [StringLength(160, ErrorMessage = "Maximum length is 160 characters.")]
+    [Display(Name = "Message")]
+    public string ?Message { get; set; }
 }
