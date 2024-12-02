86a4e174133821a31eb4d8b41f108e84a7086cc8
BBSITU <Biancabs@live.dk>
2024-12-03 00:45:50 +0100

    changes and deletions more modifications
    Co-authored-by: Caro-ITU <cslj@itu.dk>

--- src/Chirp.Infrastructure/CheepRepository.cs
+++ src/Chirp.Infrastructure/CheepRepository.cs
@@ -15,6 +15,26 @@ public class CheepRepository : ICheepRepository
         context.Database.EnsureCreated();
     }
     
+    
+    // all create methods 
+    public async Task CreateCheep(Cheep newCheep)
+    {
+        if (newCheep.Text.Length > 160) {
+            throw new ArgumentException("Cheep text is too long");
+        }
+        else {
+            _context.Add(newCheep);
+            await _context.SaveChangesAsync();;  
+        }
+        
+    }
+    public async Task CreateAuthor(Author newAuthor)
+    {
+        _context.Add(newAuthor);
+        await _context.SaveChangesAsync();
+    }
+
+    // all get methods
     public Task<Author> GetAuthorByName(string name)
     {
         var query = (from author in _context.Authors where author.UserName == name select author);
@@ -30,23 +50,7 @@ public class CheepRepository : ICheepRepository
     }
     
     
-    public async Task CreateCheep(Cheep newCheep)
-    {
-        if (newCheep.Text.Length > 160) {
-            throw new ArgumentException("Cheep text is too long");
-        }
-        else {
-          _context.Add(newCheep);
-                  await _context.SaveChangesAsync();;  
-        }
-        
-    }
-    public async Task CreateAuthor(Core.Author newAuthor)
-    {
-        _context.Add(newAuthor);
-        await _context.SaveChangesAsync();
-    }
-
+    //read and update cheep methods 
     public Task<List<CheepDTO>> ReadCheepDTO(int page)
     {
     {
--- src/Chirp.Infrastructure/ICheepRepository.cs
+++ src/Chirp.Infrastructure/ICheepRepository.cs
@@ -5,7 +5,7 @@ namespace Chirp.Infrastructure;
 public interface ICheepRepository
 {
     public Task CreateCheep(Cheep newCheep);
-    public Task CreateAuthor(Core.Author newCheep);
+    public Task CreateAuthor(Author newCheep);
     public Task<List<CheepDTO>> ReadCheepDTO(int page);
     public Task<List<CheepDTO>> ReadCheepDTOFromAuthor(int page, string authorName);
     public Task UpdateCheep(CheepDTO alteredCheep);
