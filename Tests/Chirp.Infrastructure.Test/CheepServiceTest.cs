cfb373120fd5a447894527d7706c04943b2b5af8
cslj <cslj@itu.dk>
2024-12-03 01:42:43 +0100

    set-up for test - not done

--- Tests/Chirp.Infrastructure.Test/CheepServiceTest.cs
+++ Tests/Chirp.Infrastructure.Test/CheepServiceTest.cs
@@ -6,6 +6,31 @@ namespace Chirp.Tests
 {
     public class CheepServiceTests
     {
+        private CheepService _cheepService;
+        private MockCheepRepository _mockRepository;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockRepository = new MockCheepRepository();
+            _cheepService = new CheepService(_mockRepository);
+        }
+
+        [Test]
+        public void AddCheep_ShouldAddCheep()
+        {
+            var author = new Author { UserName = "TestAuthor" , Cheeps = new List<Cheep>()};
+            var text = "Test Cheep";
+
+           
+            _cheepService.AddCheep(author, text);
+
+            
+            var cheeps = _mockRepository.ReadCheepDTO(1).Result;
+            Assert.AreEqual(1, cheeps.Count);
+            Assert.AreEqual("TestAuthor", cheeps[0].Author);
+            Assert.AreEqual("Test Cheep", cheeps[0].Text);
+        }
         
     }
 }
--- src/Chirp.Core/Author.cs
+++ src/Chirp.Core/Author.cs
@@ -1,11 +1,9 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
-
 namespace Chirp.Core;
 
 public class Author : IdentityUser
 { 
-    [Required]
     public required ICollection<Cheep> Cheeps;
     
 }
