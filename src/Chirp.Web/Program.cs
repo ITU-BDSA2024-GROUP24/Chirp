ef01bad4453aac0d22e826352b504dc3dd6c5cc5
Xander Mygind <xamy@itu.dk>
2024-12-03 14:47:44 +0100

    Reordered Program.cs and added code to register services

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -12,31 +12,17 @@ builder.Services.AddRazorPages();
 builder.Services.AddScoped<ICheepService, CheepService>();
 builder.Services.AddScoped<ICheepRepository, CheepRepository>();
 
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
+builder.Services.AddHsts(options => options.MaxAge = TimeSpan.FromDays(800));
+
 string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(connectionString));
 
 builder.Services.AddDefaultIdentity<Author>(options => 
         options.SignIn.RequireConfirmedAccount = true) 
     .AddEntityFrameworkStores<ChirpDBContext>();
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var dbContext = services.GetRequiredService<ChirpDBContext>();
-    dbContext.Database.EnsureCreated();
-    DbInitializer.SeedDatabase(dbContext); // Seed data only if necessary
-}
-
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 builder.Services.AddAuthentication(options =>
     {
@@ -47,10 +33,26 @@ builder.Services.AddAuthentication(options =>
     .AddCookie()
     .AddGitHub(o =>
     {
-        o.ClientId = builder.Configuration["authentication:github:clientId"];
-        o.ClientSecret = builder.Configuration["authentication:github:clientSecret"];
+        o.ClientId = builder.Configuration["authentication_github_clientId"] ?? string.Empty;
+        o.ClientSecret = builder.Configuration["authentication_github_clientSecret"] ?? string.Empty;
         o.CallbackPath = "/signin-github";
     });
+
+
+var app = builder.Build();
+
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
 
 app.UseDeveloperExceptionPage();
 app.UseHttpsRedirection();
@@ -63,5 +65,13 @@ app.UseAuthorization();
 app.UseSession();
 
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<ChirpDBContext>();
+    dbContext.Database.EnsureCreated();
+    DbInitializer.SeedDatabase(dbContext); // Seed data only if necessary
+}
 
 app.Run();
