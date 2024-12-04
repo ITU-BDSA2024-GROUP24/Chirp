fe97fc4d850ada40a5191c050f7e9a2d2b2b8008
fbor <fbor@itu.dk>
2024-12-04 13:58:59 +0100

    ...

---
+++ src/Chirp.Web/Pages/infopage.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using Chirp.Core;
+using Chirp.Infrastructure;
+using Chirp.Web.Pages.Shared;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Pages;
+
+public class infopage : PageModel
+{
+    
+    private readonly ICheepService _service;
+    private readonly SignInManager<Author> _signInManager;
+    required public List<CheepViewModel> Cheeps { get; set; } = new List<CheepViewModel>();
+    public required Task<Author> Author { get; set; }
+    
+    public infopage(ICheepService service, SignInManager<Author> signInManager)
+    {
+        _signInManager = signInManager;
+        _service = service;
+    }
+    
+    public ActionResult OnGet(string author, [FromQuery] int page)
+    {
+        if (page < 1)
+        {
+            return Redirect($"{Request.Path}?page=1");
+        }
+        Cheeps = _service.GetCheepsFromAuthor(page, author);
+
+        return Page();
+    }
+    public IActionResult OnPostMyMethod()
+    {
+        // Code to handle the button click goes here
+        return RedirectToPage();
+        
+    }
+    
+}
+
+
+
+
