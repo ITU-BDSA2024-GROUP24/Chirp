6887baed66ce2f8219737be04fd8ff282ca79563
BBSITU <Biancabs@live.dk>
2024-12-08 22:12:15 +0100

    follow service interface
    Co-authored-by: Caro-ITU <cslj@itu.dk>

---
+++ src/Chirp.Infrastructure/ChirpServices/FollowService.cs
@@ -0,0 +1,15 @@
+using Chirp.Core;
+using Chirp.Infrastructure.ChirpRepositories;
+
+namespace Chirp.Infrastructure.ChirpServices;
+
+public interface IFollowService
+{
+    public Task<List<FollowerDTO>> GetFollowers(string followerUser);
+    
+    public Task AddFollower(string followerUser, string followedUser);
+    
+    public Task Unfollow(string followerUser, string followedUser);
+    
+}
+
