5c93a13f241bc373f0ac33b61f36425bf2ceb599
fbor <fbor@itu.dk>
2024-12-10 14:46:05 +0100

    Aboutme page with redirect to Identety mange personaldata delete page

--- src/Chirp.Web/Pages/infopage.cshtml.cs
+++ src/Chirp.Web/Pages/infopage.cshtml.cs
@@ -1,11 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Chirp.Core;
+using Chirp.Infrastructure;
+using Chirp.Web.Pages.Shared;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Web.Pages;
 
 public class infopage : PageModel
 {
-    public void OnGet()
+    
+    private readonly ICheepService _service;
+    private readonly SignInManager<Author> _signInManager;
+    required public List<CheepViewModel> Cheeps { get; set; } = new List<CheepViewModel>();
+    public required Task<Author> Author { get; set; }
+    
+    public infopage(ICheepService service, SignInManager<Author> signInManager)
     {
-        
+        _signInManager = signInManager;
+        _service = service;
     }
+    
+    [BindProperty]
+    public string Message { get; set; }
+    
+    
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!User.Identity!.IsAuthenticated)
+        {
+            return NotFound();
+        }
+        
+        Author? author = await _service.GetAuthorByName(User.Identity.Name);
+        if (author == null)
+        {
+            return RedirectToPage("Aboutme");
+        }
+
+        await _service.AddCheep(author, Message ?? throw new NullReferenceException());
+
+        return RedirectToPage("Aboutme");
+    }
+    
+    public ActionResult OnGet(string author, [FromQuery] int page)
+    {
+        if (page < 1)
+        {
+            return Redirect($"{Request.Path}/?page=1");
+        }
+        Cheeps = _service.GetCheepsFromAuthor(page, author);
+
+        return Page();
+    }
+    
+    
 }
