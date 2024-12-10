320976ee2c03d29a9bba21304737da41c4cd20bd
BBSITU <Biancabs@live.dk>
2024-12-10 21:56:29 +0100

    new test register
    Co-authored-by: Caro-ITU <cslj@itu.dk>

--- Tests/PlaywrightTests.Test/EndToEndTest.cs
+++ Tests/PlaywrightTests.Test/EndToEndTest.cs
@@ -64,7 +64,24 @@ public class Tests : PageTest
         
     }
     
-  
+    [Test]
+    public async Task Register()
+    {
+        await Page.GotoAsync("https://bdsagroup024chirprazor.azurewebsites.net/?page=1");
+        await Page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
+        await Page.GetByPlaceholder("Name", new() { Exact = true }).ClickAsync();
+        await Page.GetByPlaceholder("Name", new() { Exact = true }).FillAsync("Playwright");
+        await Page.GetByPlaceholder("Name", new() { Exact = true }).PressAsync("Tab");
+        await Page.GetByPlaceholder("name@example.com").FillAsync("p@p.p");
+        await Page.GetByPlaceholder("name@example.com").PressAsync("Tab");
+        await Page.GetByLabel("Password", new() { Exact = true }).FillAsync("Qwe123?");
+        await Page.GetByLabel("Password", new() { Exact = true }).PressAsync("Tab");
+        await Page.GetByLabel("Confirm Password").FillAsync("Qwe123?");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "public timeline" }).ClickAsync();
+    }
+    
     [Test]
     public async Task GoToLogin()
     {
