5c5cb2b1eea427e2dd6906fb95ecd2c6c15d17ee
Xander Mygind <xamy@itu.dk>
2024-12-18 11:18:01 +0100

    Added Unit Test project and one initial Unit Test

---
+++ Tests/UnitTests.Test/UnitTests.cs
@@ -0,0 +1,31 @@
+﻿using Chirp.Core;
+using Chirp.Infrastructure;
+using Chirp.Infrastructure.ChirpRepositories;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+public class CheepRepositoryTest
+{
+
+    private static async Task<ICheepRepository> SetUpRepositoryAsync()
+    {
+        var connection = new SqliteConnection("Filename=:memory:");
+        await connection.OpenAsync();
+        var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+
+        var context = new ChirpDBContext(builder.Options);
+        await context.Database.EnsureCreatedAsync();
+
+        return new CheepRepository(context);
+    }
+
+    [Fact]
+    public async void CheckThatRepositoryIsEmptyTest()
+    {
+        var repository = await SetUpRepositoryAsync();
+        var result = await repository.ReadCheepDTO(1);
+        Assert.Empty(result);
+    }
+   
+}
